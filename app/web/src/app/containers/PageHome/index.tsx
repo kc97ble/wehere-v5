7ec57fd583d6063863904d4a3010153df960fe13
Kien Nguyen <kc97ble@gmail.com>
2025-03-04 22:50:58 +0700

    feat: initialize PageHome

---
+++ app/web/src/app/containers/PageHome/index.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import styles from './index.module.scss';
+
+export const PageHome = () => {
+  return (
+    <div className={styles.container}>
+      <h1>Home Page</h1>
+    </div>
+  );
+};
+
+export default PageHome;
